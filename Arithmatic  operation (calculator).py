8114cedad7402b6ffb55c3ba87b2490c6ffa7158
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:09:54 +0530

    Add files via upload

---
+++ Arithmatic  operation (calculator).py
@@ -0,0 +1,7 @@
+print(3+2)       #sum(+)
+print(4-2)       #substraction(-)
+print(3*4)       #multiplication(*)
+print(9/3)       #divison(/)
+print(5//4)      #floor divison..it will be 1.25 but output willbe 1
+print(2**2**3)   #exponential(**)
+print(8%4)       #modulus(%)...it gives remainder
